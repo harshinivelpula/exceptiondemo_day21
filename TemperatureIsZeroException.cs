ae9fe758760454d265843628d170579e7aaa6c2d
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-11-04 20:25:59 +0530

    temp exception

---
+++ TemperatureIsZeroException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionDemo
+{
+    public class TemperatureIsZeroException:Exception
+    {
+        public TemperatureIsZeroException(string message):base(message)
+        {
+
+        }
+    }
+}
