0dd0626fdb7d5659e067eb47f76cf0c17fab1310
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-11-04 20:25:19 +0530

    add temp

---
+++ Temperature.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionDemo
+{
+    public class Temperature
+    {
+        int temperature = 0;
+        public void ShowTemp()
+        {
+            if(temperature==0)
+            {
+                throw new TemperatureIsZeroException("Temperature is zero");
+            }
+            else
+            {
+                Console.WriteLine("Temperature is" + temperature);
+            }
+        }
+    }
+}
